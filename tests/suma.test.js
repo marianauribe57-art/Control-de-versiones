139c3019c3638850bd6d402e5475747296d2e2cc
Mariana Uribe <marianauribe57@gmail.com>
2026-03-11 14:16:22 -0500

    actualización suma

--- tests/suma.test.js
+++ tests/suma.test.js
@@ -3,6 +3,6 @@ import assert from 'node:assert/strict';
 function suma(a, b) {
 return a + b;
 }
-test('suma 2 + 3 = 5', () => {
-assert.equal(suma(2, 3), 5);
+test('suma 3 + 3 = 6', () => {
+assert.equal(suma(3, 3),6);
 });
